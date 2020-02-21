8bdf85850677c23ece90ab77494cbb382786a58a
Pablo Vega <pablo.vega@tiendeo.com>
2020-02-21 07:09:38 +0100

    Move teamMembersTable to containers

--- src/containers/WoffuBoard/index.js
+++ src/containers/WoffuBoard/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header } from '../../components/header';
-import { TeamMembersTable } from '../../components/teamMembersTable';
+import { TeamMembersTable } from '../teamMembersTable';
 
 export const WoffuBoard = () => {
   return (
